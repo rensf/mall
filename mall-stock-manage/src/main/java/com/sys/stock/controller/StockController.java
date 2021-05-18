574979bcc0867fcccc27d5d3c08fd8a18a2d1a92
rensf <2636618553@qq.com>
2021-05-18 16:07:54 +0800

    配置中心搭配消息总线实现动态刷新
    Signed-off-by: rensf <2636618553@qq.com>

--- mall-stock-manage/src/main/java/com/sys/stock/controller/StockController.java
+++ mall-stock-manage/src/main/java/com/sys/stock/controller/StockController.java
@@ -1,8 +1,6 @@
 package com.sys.stock.controller;
 
 import io.swagger.annotations.Api;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -14,15 +12,5 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/stock")
 @Api("库存管理")
 public class StockController {
-
-    /**
-     * 测试获取配置中心配置信息
-     */
-    @Value("${mysql-url}")
-    private String mysqlUrl;
-    @GetMapping("/test")
-    public String testConfig() {
-        return mysqlUrl;
-    }
 
 }
